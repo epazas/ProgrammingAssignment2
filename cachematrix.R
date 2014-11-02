1f3f4c8d5014b36759cd865f2f53e1125b46cf8b
Ilias Pazas <epaz.yp@hotmail.com>
2014-11-02 23:38:41 +0200

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are 2 slightly different implementations
+## The first (cacheSolve) returns the inverse 
+## of a given matrix returning a cached value, if exists.
+## If not, performs the calculation and stores the result
+## The second (cacheSolveEx) returns the inverse 
+## of a given matrix returning a cached value, if exists.
+## If not, performs the calculation and stores the result
+## Apart from checking for a cached value, it performs an
+## equality check with the given original matrix that was
+## initially used for the calculation
 
-## Write a short comment describing this function
+## Calculates and stores the inverse of a matrix, without
+## making validity checks on the input
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of a matrix, checking
+## for an existing cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting inverse from cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
+
+## Returns the inverse of a matrix, checking
+## for an existing cached value
+## Performs an extra check against the given y input that 
+## points to the original matrix used in construction of x 
+
+cacheSolveEx <- function(x, y, ...) {  
+  data <- x$get()
+  if (matequal(data, y)){
+    m <- x$getinv()
+    if(!is.null(m)) {
+      message("getting inverse from cached data")
+      return(m)
+    }
+  }
+  else
+    message("original matrix has been changed, recalculating")
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
+
+## Calculates equality check between 2 matrices
+
+matequal <- function(x, y){
+  is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
 }
